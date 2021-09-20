1f6bf73c3c7bc62ebf8ceb6b425e3037f95f56b8
Joe Ma <63757906+jmBSU@users.noreply.github.com>
2021-09-19 20:33:43 -0600

    Fixed off by one issue
    This fixed the scanline issue

--- bar.R
+++ bar.R
@@ -19,8 +19,8 @@ ani.options(interval = 1/fps);
 #Notation of b is in (y,x)
 b = read.table(file = "battxt.txt", strip.white = TRUE)
 
-totalFrames = floor(dim(b)[1]/d.y);
 d.y = (dim(b)[2]/4)*3; #Dimension of y - assumes the data set takes a 4:3 AR
+totalFrames = floor(dim(b)[1]/d.y);
 
 ani.record(reset = TRUE);#Clears queue
 dev.control('enable');
@@ -28,16 +28,16 @@ dev.control('enable');
 
 saveVideo({
   for (i in (1:totalFrames)) {
-    yStart = (i * d.y) - (d.y - 1);#Finds next frame and starts at the top of the frame
+    yStart = (i * d.y) - (d.y);#Finds next frame and starts at the top of the frame
     frame(); #Creates a new plot
     plot.window(c(1, dim(b)[2]), c(1, d.y));
     par(bg = "white");
-    while(yStart <= i * d.y){ #Cuz R indexes at 1
+    while(yStart < i * d.y){ #Cuz R indexes at 1
       xStart = 1;
       while(xStart <= dim(b)[2]){
         
-        if(b[yStart,xStart] != 0){#Creates a black rect at every 255 spot
-          rect(xStart - dx, d.y - yStart %% (d.y + 1) - dy,xStart + dx,d.y - yStart %% (d.y + 1) + dy, col = "black")
+        if(b[yStart+1,xStart] != 0){#Creates a black rect at every 255 spot
+          rect(xStart - dx, d.y - yStart %% (d.y) - dy,xStart + dx,d.y - yStart %% (d.y) + dy, col = "black")
         }#Since we start at top of frame, y position is subtracted since graphs don't start from the top.
         
         xStart = xStart + 1;
